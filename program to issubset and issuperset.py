16e1c835729256ae0771651f8c7a0fa90fd5b8ff
Aman-johri <60850006+Aman-johri@users.noreply.github.com>
2020-04-19 00:36:27 +0530

    program to issubset and issuperset

---
+++ program to issubset and issuperset.py
@@ -0,0 +1,8 @@
+# program to issubset and issuperset
+s1=set(["apple","mango"])
+s2=set(["mango","orange"])
+s3=set(["mango"])
+issubset= s1>=s2
+print(issubset)
+issuperset=s1<=s2
+print(issuperset)
