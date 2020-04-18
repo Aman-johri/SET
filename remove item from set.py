8bebce86626daf3704d64257be393f2e81c09899
Aman-johri <60850006+Aman-johri@users.noreply.github.com>
2020-04-19 00:36:57 +0530

    remove item from set

---
+++ remove item from set.py
@@ -0,0 +1,11 @@
+#program to remove items from set
+# process 1
+s={1,23,4,"red","green"}
+s.remove("red")
+print(s)
+
+#process 2
+#pop takes no orgument
+x={1,2,3,4,"red","green"}
+x.pop()
+print(x)
